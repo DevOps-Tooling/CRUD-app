5eed7e10befd808b27aaf03da08519a3e863032b
Kant <Kant.Krishan@bcg.com>
2021-05-23 17:31:40 +0530

    Add Crud App

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const url = 'mongodb://host.docker.internal/AlienDB'
+const app = express()
+
+
+
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', function(){
+
+    console.log('connected...')
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routers/aliens')
+app.use('/aliens',alienRouter)
+
+
+app.listen(9000,function(){
+    console.log('Server connected')
+})
