40321eda06444dd5434bf02bd5a216ab26dc6383
Kant <Kant.Krishan@bcg.com>
2021-05-25 16:26:24 +0530

    app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const url = 'mongodb://host.docker.internal/AlienDB'
+const url = 'mongodb://admin:admin@cluster0-shard-00-00.kgid7.mongodb.net:27017,cluster0-shard-00-01.kgid7.mongodb.net:27017,cluster0-shard-00-02.kgid7.mongodb.net:27017/AlienDB?ssl=true&replicaSet=atlas-yd7prh-shard-0&authSource=admin&retryWrites=true&w=majority'
 const app = express()
-
-
 
 
 mongoose.connect(url, {useNewUrlParser:true})
